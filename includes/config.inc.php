068c3eb9c7122a34e1312c7653b37205e50604a9
danyadigergo <82826278+danyadigergo@users.noreply.github.com>
2021-04-20 19:59:12 +0200

    Add files via upload

---
+++ includes/config.inc.php
@@ -0,0 +1,31 @@
+<?php
+$ablakcim = array(
+    'cim' => 'Csak Előre!',
+);
+
+$fejlec = array(
+	'kepforras' => 'select-mundo-purple-orange-1-es-meret.jpg',
+    'kepalt' => 'labda',
+    'cim' => 'Csak Előre!',
+	'motto' => ''
+);
+
+$lablec = array(
+    'copyright' => 'Copyright '.date("Y").'.',
+    'ceg' => 'Csak Előre! A Szegedi Kézilabdáért Egyesület'
+);
+
+$oldalak = array(
+	'/' => array('fajl' => 'cimlap', 'szoveg' => 'Rólunk', 'menun' => array(1,1)),
+	'galeria' => array('fajl' => 'galeria', 'szoveg' => 'Galéria', 'menun' => array(1,1)),
+	'kapcsolat' => array('fajl' => 'kapcsolat', 'szoveg' => 'Kapcsolat', 'menun' => array(1,1)),
+	'hirek' => array('fajl' => 'cikkek', 'szoveg' => 'Hírek', 'menun' => array(1,1)),
+    'versenynaptar' => array('fajl' => 'versenynaptar', 'szoveg' => 'Versenynaptár', 'menun' => array(1,1)),
+    'belepes' => array('fajl' => 'belepes', 'szoveg' => 'Belépés', 'menun' => array(1,0)),
+    'kilepes' => array('fajl' => 'kilepes', 'szoveg' => 'Kilépés', 'menun' => array(0,1)),
+    'belep' => array('fajl' => 'belep', 'szoveg' => '', 'menun' => array(0,0)),
+    'regisztral' => array('fajl' => 'regisztral', 'szoveg' => '', 'menun' => array(0,0))
+);
+
+$hiba_oldal = array ('fajl' => '404', 'szoveg' => 'A keresett oldal nem található!');
+?>
---
+++ templates/pages/versenynaptar.tpl.php
@@ -0,0 +1,34 @@
+<link rel="stylesheet" href="./styles/tablazat.css" type="text/css">
+<table>
+    <caption>Versenynaptár</caption>
+    <tr>
+        <th>Dátum</th>
+        <th>Ellenfél</th>
+        <th>Helyszín</th>
+        <th>Versenysorozat</th>
+    </tr>
+    <tr>
+        <td>Március 31.</td>
+        <td>THW-Kiel</td>
+        <td>Szeged</td>
+        <td>EHF BL</td>
+    </tr>
+    <tr>
+        <td>Április 7.</td>
+        <td>THW-Kiel</td>
+        <td>Kiel</td>
+        <td>EHF BL</td>
+    </tr>
+    <tr>
+        <td>Április 11.</td>
+        <td>Veszprémi KKFT</td>
+        <td>Szigetszentmiklós</td>
+        <td>Magyar Kupa</td>
+    </tr>
+    <tr>
+        <td>Április 12.</td>
+        <td>Telekom Veszprém</td>
+        <td>Szigetszentmiklós</td>
+        <td>Magyar Kupa</td>
+    </tr>
+</table>
